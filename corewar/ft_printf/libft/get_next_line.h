5f4368d9a53b8e872404456f05d76072fad2e65e
Meagan Desmond redwyne <medesmon@ga-u1.21-school.ru>
2020-01-17 19:22:18 +0300

    CV and Corewar rdy

--- corewar/ft_printf/libft/get_next_line.h
+++ corewar/ft_printf/libft/get_next_line.h
@@ -3,26 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smanhack <smanhack@student.42.fr>          +#+  +:+       +#+        */
+/*   By: medesmon <medesmon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/08/23 13:40:45 by smanhack          #+#    #+#             */
-/*   Updated: 2019/10/29 13:16:57 by smanhack         ###   ########.fr       */
+/*   Created: 2019/01/21 05:16:52 by medesmon          #+#    #+#             */
+/*   Updated: 2020/01/17 17:26:34 by medesmon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 100
-# include <libc.h>
+# define BUFF_SIZE 1
+# define FD_MAX_NUM 4864
+
 # include "libft.h"
 
-typedef struct		s_file
-{
-	int				fd;
-	char			*line;
-}					t_file;
-
-int					get_next_line(const int fd, char **line);
+int		get_next_line(const int fd, char **line);
 
 #endif
